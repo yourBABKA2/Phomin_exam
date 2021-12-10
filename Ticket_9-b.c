a19124991501a86aa5b13945fb22bebbb7a0e3f0
yourBABKA2 <72077298+yourBABKA2@users.noreply.github.com>
2021-12-11 02:42:55 +0300

    Add files via upload

--- Ticket_9-b.c
+++ Ticket_9-b.c
@@ -4,11 +4,9 @@
     4 чтение исходных данных в массив выполнять из подготовленного заранее текстового файла 
     5 осуществить поиск нужного курса, и указать в какой день он был установлен
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
 
 
 int main()
@@ -30,13 +28,23 @@ int main()
     #define coeff_hi 50.0f
     float coeff[days];
 
-    for (int i = 0; i < days; i++)
-    {
-        coeff[i] = coeff_lo + (coeff_hi - coeff_lo) * rand() / RAND_MAX;
-    }
+    FILE *f;
+    f = fopen("Ticket9.txt","w");
 
     for (int i = 0; i < days; i++)
     {
+        coeff[i] = coeff_lo + (coeff_hi - coeff_lo) * rand() / RAND_MAX;
+        fprintf(f, "%.2f\n", coeff[i]);
+    }
+
+    if (days % 7 > 0)
+        fprintf(f, "\n");
+    fclose(f);
+
+    f = fopen("Ticket9.txt","r");
+    for (int i = 0; i < days; i++)
+    {
+        fscanf(f, "%.2f", &coeff[i]);
         printf("% 6.2f", coeff[i]);
         if (i % 7 == 6)
             printf("\n");
@@ -59,6 +67,6 @@ int main()
     printf("\n");
     printf("min = %.2f\n", cmin);
     printf("max = %.2f\n", cmax);
-    
+
     return 0;
 }
